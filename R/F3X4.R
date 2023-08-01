095d1c550cce94392a646256bd32f14d5f84ebf3
fanyue322 <34554433+fanyue322@users.noreply.github.com>
2023-08-01 15:57:16 +0800

    Update F3X4.R

--- R/F3X4.R
+++ R/F3X4.R
@@ -4,11 +4,11 @@ bf_by_codon <- function(x) {
   y <- codon2dna(x)
   ny <- sum(attr(y, "weight"))
   M <- matrix(0, nrow = 4, ncol = 3)
-  M[, 1] <- subset(y, select = seq(1, ny, by = 3), site.patter = FALSE) |>
+  M[, 1] <- subset(y, select = seq(1, ny, by = 3), site.patter = FALSE) %>%
     baseFreq()
-  M[, 2] <- subset(y, select = seq(2, ny, by = 3), site.patter = FALSE) |>
+  M[, 2] <- subset(y, select = seq(2, ny, by = 3), site.patter = FALSE) %>%
     baseFreq()
-  M[, 3] <- subset(y, select = seq(3, ny, by = 3), site.patter = FALSE) |>
+  M[, 3] <- subset(y, select = seq(3, ny, by = 3), site.patter = FALSE) %>%
     baseFreq()
   M
 }
@@ -16,8 +16,8 @@ bf_by_codon <- function(x) {
 # return frequencies for all 61 states
 F3x4_freq <- function(M, CodonAlphabet,
                       nucleotideAlphabet = .nucleotideAlphabet) {
-  pos <- CodonAlphabet |> strsplit("") |> unlist() |>
-    match(nucleotideAlphabet) |> matrix(ncol = 3, byrow = TRUE)
+  pos <- CodonAlphabet %>% strsplit("") %>% unlist() %>%
+    match(nucleotideAlphabet) %>% matrix(ncol = 3, byrow = TRUE)
   codon_frequencies <- M[pos[, 1], 1] * M[pos[, 2], 2] * M[pos[, 3], 3]
   codon_frequencies / sum(codon_frequencies)
 }
@@ -31,7 +31,7 @@ F3x4 <- function(x) {
 
 
 F1x4 <- function(x, bf=NULL) {
-  if(is.null(bf))bf <- codon2dna(x) |> baseFreq()
+  if(is.null(bf))bf <- codon2dna(x) %>% baseFreq()
   BF <- matrix(bf, 4, 3)
   codon_abc <- attr(x, "levels")
   F3x4_freq(BF, CodonAlphabet = codon_abc)
